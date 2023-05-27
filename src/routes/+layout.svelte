aae5f04ddd5fa5320a8b50222d4b9738686be81b
Luc Zbonack <lmzbonack@gmail.com>
2023-05-26 21:32:38 -0700

    auth and oauth

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import '../styles.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			invalidate('supabase-auth');
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<div class="container" style="padding: 50px 0 100px 0">
+	<slot />
+</div>
